f655258e1a7801fe0f59c29661177cc4638e0682
garricksjakob@gmail.com <garricksjakob@gmail.com>
2018-04-10 12:31:35 -0700

    added deposit

--- model.py
+++ model.py
@@ -8,6 +8,7 @@ import csv
 class Model():
     def __init__(self):
         super().__init__()
+        self.fieldnames = ['First Name', 'Last Name', 'Account Number', 'PIN', 'Balance']
         self.accounts = {}
 
     def read_file(self):
@@ -15,19 +16,28 @@ class Model():
             with open('CLI/accounts.csv', 'r') as open_f:
                 csv_reader = csv.DictReader(open_f)
                 for user in csv_reader:
-                    self.accounts[user['Account Number']] = {'First Name': user['First Name'], 'Last Name': user['Last Name'], 'PIN': user['PIN'], 'Balance':user['Balance']}
+                    self.accounts[user['Account Number']] = {'First Name': user['First Name'], 'Last Name': user['Last Name'], 'PIN': user['PIN'], 'Balance':int(user['Balance'])}
 
             #print(self.accounts)
         except FileNotFoundError:
             self.write_file()
 
     def write_file(self):
-        pass
+        with open('CLI/accounts.csv', 'w') as open_f:
+            csv_writer = csv.DictWriter(open_f, fieldnames=self.fieldnames, delimiter=',')
+            csv_writer.writeheader()
+            for user, value in self.accounts.items():
+                dict = {'Account Number': user,
+                        'First Name': value['First Name'],
+                        'Last Name': value['Last Name'],
+                        'PIN': value['PIN'],
+                        'Balance': value['Balance']
+                }
+                csv_writer.writerow(dict)
 
-    def makeNew(self , name):
+    def makeNew(self , name, bal):
+        self.user = Account(name, bal)
 
-        user = Account(name)
-        print(user)
 
 
 
