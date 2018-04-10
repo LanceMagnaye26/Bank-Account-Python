6e5a358b89e94713f1b7230800d7be7d1bdbd706
garricksjakob@gmail.com <garricksjakob@gmail.com>
2018-04-09 22:37:09 -0700

    i feel like i broke somthing

---
+++ controler.py
@@ -0,0 +1,9 @@
+from views.view1 import ATM
+
+from tkinter import *
+from tkinter import messagebox
+from tkinter.filedialog import askopenfilename
+import os
+
+class Constroller:
+    def __init__(self):
